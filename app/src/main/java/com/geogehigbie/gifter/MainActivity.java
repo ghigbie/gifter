c9bbf7efff73f467170348df1202a08c94a8bade
George Higbie <gehigbie@gmail.com>
2016-11-26 17:12:29 -0700

    modifed layout and began fragments

--- app/src/main/java/com/geogehigbie/gifter/MainActivity.java
+++ app/src/main/java/com/geogehigbie/gifter/MainActivity.java
@@ -1,13 +1,26 @@
 package com.geogehigbie.gifter;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fragmentInitialize(){
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.
+
+
     }
 }
